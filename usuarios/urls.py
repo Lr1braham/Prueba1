4e4590823759b932f022434135ddd8443c144942
Lr1braham <pongut@gmail.com>
2025-09-22 15:55:32 -0500

    vercontactos2

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,11 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),           # Página de inicio
     path('contact/', views.contact_view, name='contact'),  # Formulario
     path('success/', views.success_view, name='success'),  # Éxito
-    path("login/", views.login_view, name="login"),
+    path("login/", views.login_view, name="login"), # página de login
+    path("contacts/", views.contact_list, name="contact_list"),# lista de contactos
 ]
-
-# myapp/urls.py
-from django.urls import path
-from . import views
-
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 # en myapp/views.py
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from .models import Contact
 
 def home(request):
     return render(request, "usuarios/home.html")
@@ -19,9 +20,14 @@ def contact_view(request):
 def success_view(request):
     return render(request, 'usuarios/success.html')
 
-#aaaaaaaaa
-
 def login_view(request):
     return render(request, "usuarios/login.html")  # si creas login.html
 
+#aaaaaaaaa
+def contact_list(request):
+    contacts = Contact.objects.all()  # SELECT * FROM contact;
+    return render(request, "Contact.html", {"form": form})# lista de contactos
 
+def login_view(request):
+    contacts = Contact.objects.all()  # Trae todos los registros
+    return render(request, "Login.html", {"contacts": contacts})
