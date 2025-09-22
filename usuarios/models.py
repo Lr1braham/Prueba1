08ad40d1fcf3749802dc45ca75f451571d4ee1c8
Lr1braham <pongut@gmail.com>
2025-09-22 00:00:43 -0500

    phon and subjet

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,6 +4,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
+    phone = models.CharField(max_length=20, blank=True, null=True)   # ✅ nuevo campo
+    subject = models.CharField(max_length=200, blank=True, null=True) # ✅ nuevo campo
 
     def __str__(self):
         return f"{self.name} - {self.email}"
