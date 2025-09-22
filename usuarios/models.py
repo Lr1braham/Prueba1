f31d682df74052c2bea3eea1ba5fd649ef4cc043
Lr1braham <pongut@gmail.com>
2025-09-22 00:12:33 -0500

    campos eliminados

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
+    
     email = models.EmailField()
-    message = models.TextField()
-    phone = models.CharField(max_length=20, blank=True, null=True)   # ✅ nuevo campo
-    subject = models.CharField(max_length=200, blank=True, null=True) # ✅ nuevo campo
+    #name = models.CharField(max_length=100)
+    #message = models.TextField()
+    #phone = models.CharField(max_length=20, blank=True, null=True)   # ✅ nuevo campo
+    #subject = models.CharField(max_length=200, blank=True, null=True) # ✅ nuevo campo
+    password = models.CharField(max_length=128, default="123456")# campo de contraseña
 
     def __str__(self):
         return f"{self.name} - {self.email}"
