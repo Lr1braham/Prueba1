ae9c475e74047e86f7d936f99505cf28d5e1620b
Lr1braham <pongut@gmail.com>
2025-09-22 02:48:45 -0500

    logo background2

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,8 +1,25 @@
 # en myapp/forms.py
+# myapp/forms.py
 from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['email','password']  # Incluye el campo de contraseña
+        fields = ['email', 'password']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control shadow-sm rounded-lg',
+                'placeholder': 'Correo electrónico',
+                'required': True,
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'form-control shadow-sm rounded-lg',
+                'placeholder': 'Contraseña',
+                'required': True,
+            }),
+        }
+        labels = {
+            'email': 'Correo electrónico',
+            'password': 'Contraseña',
+        }
