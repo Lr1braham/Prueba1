0e3d2ce26d66c6e0213538514caa8ef75ead1a7b
Lr1braham <pongut@gmail.com>
2025-09-22 01:15:28 -0500

    login contact memoria 2

--- Lovely/settings.py
+++ Lovely/settings.py
@@ -153,9 +153,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]  # si tienes una carpeta "static" en el proyecto
+STATIC_ROOT = BASE_DIR / "staticfiles"    # para producción (collectstatic)
+
