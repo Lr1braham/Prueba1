72c58e6a0ecd20db84c72f3d166309b2ec74064a
Lr1braham <pongut@gmail.com>
2025-09-24 00:28:27 -0500

    registernew2

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,7 +4,8 @@ from .views import login_view
 
 urlpatterns = [
     path('', views.home, name='home'),           # Página de inicio
-    path('contact/', views.contact_view, name='contact'),  # Formulario
+    #path('contact/', views.contact_view, name='contact'),  # Formulario
+    path("contact/", views.register_view, name="contact"),  # ✅ ahora usa register_view
     path('success/', views.success_view, name='success'),  # Éxito
     path("dashboard/", views.dashboard_view, name="dashboard"),# página protegida
     path("logout/", views.logout_view, name="logout"), # página de logout
